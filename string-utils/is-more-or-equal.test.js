81dd9a8e3399089ad58768aff694b7b4958d3a14
Alex S <alex.frml1709@gmail.com>
2026-07-09 17:55:02 +0500

    Реализована функция isMoreOrEqual через TDD, возвращающая true, если a >= b

---
+++ string-utils/is-more-or-equal.test.js
@@ -0,0 +1,33 @@
+import { test, describe, expect} from "bun:test"
+import { isMoreOrEqual } from "./is-more-or-equal"; 
+
+
+describe("Тест функциии isMoreOrEqual", () => {
+    test("Должен вернуть true при 'cat' и 'car'", () => {
+        expect(isMoreOrEqual("cat", "car")).toBe(true)
+    });
+
+    test("Должен вернуть true при 'hello' и 'hello'", () => {
+        expect(isMoreOrEqual("hello", "hello")).toBe(true)
+    });
+
+    test("Должен вернуть false при 'car' и 'cat'", () => {
+        expect(isMoreOrEqual("car", "cat")).toBe(false)
+    });
+
+    test("Должен вернуть true при 'hello!' и 'hello'", () => {
+        expect(isMoreOrEqual("hello!", "hello")).toBe(true)
+    });
+
+    test("Должен вернуть false при 'hello' и 'hello!'", () => {
+        expect(isMoreOrEqual("hello", "hello!")).toBe(false)
+    });
+    
+    test("Должен вернуть true при '' и ''", () => {
+        expect(isMoreOrEqual("", "")).toBe(true)
+    });
+
+    test("Должен выбросить TypeError если хотя бы один из аргументов не строка", () => {
+        expect(() => isMoreOrEqual(123, "aaa")).toThrow(TypeError)
+    });
+});

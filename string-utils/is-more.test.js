cf80fcb8542d4590027f5bc739f39c12374ee24b
Alex S <alex.frml1709@gmail.com>
2026-07-10 17:53:07 +0500

    Добавил все файлы проекта ещё раз

---
+++ string-utils/is-more.test.js
@@ -0,0 +1,53 @@
+import { test, describe, expect} from "bun:test"
+import { isMore } from "./is-more"
+
+
+describe("Тест функциии isMore", () => {
+    test("Должен вернуть true при 'cat' и 'car'", () => {
+        expect(isMore("cat", "car")).toBe(true)
+    });
+
+    test("Должен вернуть false при 'car' и 'cat'", () => {
+        expect(isMore("car", "cat")).toBe(false)
+    });
+
+    test("Должен вернуть false при 'hello' и 'hello'", () => {
+        expect(isMore("hello", "hello")).toBe(false)
+    });
+
+    test("Должен вернуть true при 'hello!' и 'hello'", () => {
+        expect(isMore("hello!", "hello")).toBe(true)
+    });
+
+    test("Должен вернуть false при 'hello' и 'hello!'", () => {
+        expect(isMore("hello", "hello!")).toBe(false)
+    });
+    
+    test("Должен вернуть true при 'b' и 'aaaa'", () => {
+        expect(isMore("b", "aaaa")).toBe(true)
+    });
+    
+    test("Должен вернуть false при 'Admin' и 'admin'", () => {
+        expect(isMore("Admin", "admin")).toBe(false)
+    });
+    
+    test("Должен вернуть false при ' a' и 'aa'", () => {
+        expect(isMore(" a", "aa")).toBe(false)
+    });
+    
+    test("Должен вернуть true при 'aaa ' и 'aaa'", () => {
+        expect(isMore("aaa ", "aaa")).toBe(true)
+    });
+    
+    test("Должен вернуть false при '' и ''", () => {
+        expect(isMore("", "")).toBe(false)
+    });
+
+    test("Должен вернуть false при '' и 'a'", () => {
+        expect(isMore("", "a")).toBe(false)
+    });
+
+    test("Должен выбросить TypeError если хотя бы один из аргументов не строка", () => {
+        expect(() => isMore(123, "aaa")).toThrow(TypeError)
+    });
+});
---
+++ string-utils/is-not-equal.js
@@ -0,0 +1,7 @@
+export function isNotEqual(a, b) {
+    if (typeof a !== "string" || typeof b !== "string") {
+        throw new TypeError("Оба аргумента должны быть строками");        
+    }
+
+    return a !== b
+}

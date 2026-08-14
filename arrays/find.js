a98972e518408f9c6dc84326a19e93e22b40db84
Alex S <alex.frml1709@gmail.com>
2026-08-14 14:22:38 +0500

    Реализованы find и findIndex — поиск элемента и индекса с ранним выходом

---
+++ arrays/find.js
@@ -0,0 +1,21 @@
+import { findIndex } from "./findIndex";
+
+
+/**
+ * Если удовлетворяет условию, функция возвращает элемент исходного массива (не мутирует массив)
+ * @param {Array<*>} arr - исходный массив 
+ * @param {function(*, number, Array<*>): true} callback - функция, вызываемая до первого вхождения элемента
+ * @returns {*} элемент исходного массива
+ * @throws {TypeError} - если arr не массив или callback не функция
+ */
+
+
+export function find(arr, callback) {
+    const arrIndex = findIndex(arr, callback);
+
+    if (arrIndex === -1) {
+        return undefined;
+    }
+
+    return arr[arrIndex];
+}
---
+++ arrays/findIndex.js
@@ -0,0 +1,31 @@
+import { len } from "./len";
+
+
+/**
+ * Функция возвращает индекс первого вхождения элемента в исходный массив (не мутирует массив)
+ * @param {Array<*>} arr - исходный массив 
+ * @param {function(*, number, Array<*>): true} callback - функция, вызываемая до первого вхождения элемента
+ * @returns {number} индекс первого вхождения элемента
+ * @throws {TypeError} - если arr не массив или callback не функция
+ */
+
+
+export function findIndex(arr, callback) {
+    if (!Array.isArray(arr)) {
+        throw new TypeError("arr должен быть массивом")
+    }
+
+    if (typeof callback !== "function") {
+        throw new TypeError("callback должен быть функцией")
+    }
+
+    const arrLen = len(arr);
+
+    for (let i = 0; i < arrLen; i++) {
+        if (callback(arr[i], i, arr)) {
+            return i;
+        }
+    }
+
+    return -1;
+}

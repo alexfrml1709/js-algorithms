af4275aa66df7ef088fd2a6cbd99116ae4e80a51
Alex S <alex.frml1709@gmail.com>
2026-08-15 17:06:41 +0500

    Реализована flat — разглаживание вложенных массивов

---
+++ arrays/flat.js
@@ -0,0 +1,36 @@
+import { concat } from "./concat"
+import { reduce } from "./reduce"
+
+
+/**
+ * Функция сглаживает исходный массив до определенной глубины depth (не мутирует исходный массив)
+ * @param {Array<*>} arr - исходный массив 
+ * @param {number} depth - глубина разглаживания
+ * @returns {Array<*>} разглаженный массив
+ * @throws {TypeError} - если arr не массив или depth не число
+ * @example
+ *     flat([1, [2]]); // [1, 2]
+ *     flat([1, [2, [3, [4]], 5]], 2); // [1, 2, 3, [4], 5]
+ */
+
+
+export function flat(arr, depth = 1) {
+    if (!Array.isArray(arr)) {
+        throw new TypeError("arr должен быть массивом")
+    }
+
+    if (typeof depth !== "number") {
+        throw new TypeError("depth должен быть числом")
+    }
+
+    return reduce(arr, (acc, element, index) => {
+        if (Array.isArray(element) && depth > 0) {
+            const flatResult = flat(element, depth - 1)
+            acc = concat(acc, flatResult);
+        } else {
+            acc = concat(acc, [element]);
+        }
+
+        return acc;
+    }, [])
+}

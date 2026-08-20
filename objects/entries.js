df694bf9eb74f734b74dabfb90f137a088ebee72
Alex S <alex.frml1709@gmail.com>
2026-08-20 13:53:10 +0500

    Добавлен JSDoc к функции entries

--- objects/entries.js
+++ objects/entries.js
@@ -2,6 +2,16 @@ import { len } from "../arrays/len";
 import { keys } from "./keys";
 
 
+/**
+ * Функция возвращает массив массивов ключей и значений исходного объекта (не мутирует исходный объект)
+ * @param {Object} obj - исходный объект 
+ * @returns {Array<*>} массив массивов ключей и значений объекта
+ * @throws {TypeError} - если obj является null или obj не объект
+ * @example
+ *     entries({a: 1, b: 2, c: 3}); // [["a", 1], ["b", 2], ["c", 3]]
+ */
+
+
 export function entries(obj) {
     if (obj === null) {
         throw new TypeError("obj не должен равняться null")
